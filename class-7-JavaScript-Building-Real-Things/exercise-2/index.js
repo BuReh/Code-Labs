641281593492414c8943a344ee01d10dec9b63bb
BuReh <rehbu23@g-apps.monett.k12.mo.us>
2022-09-19 14:19:23 -0500

    add class 5

--- class-7-JavaScript-Building-Real-Things/exercise-2/index.js
+++ class-7-JavaScript-Building-Real-Things/exercise-2/index.js
@@ -1,1 +1,13 @@
-console.log("hi");
+
+let colors = ['red', 'orange', 'green', 'blue']
+let num = Math.floor(Math.random() * 5);
+let num2 = Math.floor(Math.random() * 5);
+
+
+function changeColor() {
+    document.body.style.backgroundColor = colors[num]
+    document.getElementById("title").style.color = colors[num2]
+}
+
+
+
